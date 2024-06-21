f41969da92be9887cd9f8ed802b2fb9542769b7e
KhushbuPie <khushbudesai624@digipie.net>
2024-06-21 17:23:12 +0530

    Advance Model Relations

--- Blogging_Platform/personal_blog/blog/management/commands/refresh_materialized_view.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from django.db import connection
-
-class Command(BaseCommand):
-    help = 'Refresh materialized view for post with details'
-
-    def handle(self, *args, **kwargs):
-        with connection.cursor() as cursor:
-            cursor.execute('REFRESH MATERIALIZED VIEW post_with_details')
